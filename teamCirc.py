be2efb32edf76ec7e391d6844879c760ab8a5154
LevStar <lev.fruchter@uamaker.nyc>
2019-03-19 16:55:20 -0400

    1st file

---
+++ teamCirc.py
@@ -0,0 +1,13 @@
+# 2nd try at a team branch/merge project
+
+from graphics import *
+
+def draw_circ(cX, cY, cRad, cCol, cWin):
+    circ = Circle(cX, cY, cRad)
+    circ.setFill(cCol)
+    circ.draw(cWin)
+
+ccWin = GraphWin("Team Circ", 500, 500)
+ccWin.setCoords(0, 0, 500, 500)
+
+draw_circ(250, 250, 50, "red", ccWin)
